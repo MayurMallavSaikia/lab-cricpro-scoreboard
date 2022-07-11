44d06e9084c53dff6669ce697e4ec4aa5c8221d4
Mayur Mallav Saikia <89935072+MayurMallavSaikia@users.noreply.github.com>
2022-07-11 20:09:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
   function display(num){
   
-             document.getElementById('img'+num).innerHTML="<img src='/assets/player"+num+".png'>";
+             document.getElementById('img'+num).innerHTML="<img src='assets/player"+num+".png'>";
         }
 
-        function myFunction() {
-                var popup = document.getElementById("myPopup");
-                popup.classList.toggle("show");
-              }
